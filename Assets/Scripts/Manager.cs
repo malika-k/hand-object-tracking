172a8461ea675aadf17e4dbcd0f1d4667623744d
malika-k <mkrkodial@gmail.com>
2021-04-01 00:13:02 -0400

    used Time.deltaTime and frameRate to animate hands without slider

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -7,53 +7,67 @@ public class Manager : MonoBehaviour
 {
     public GameObject handleData;
     public GameObject moveBones;
-    public Text frameText;
-    public int numFrames;
-    public int frame;
-    public float interval;
-    public FrameList frameInfo;
     public Slider frameSlider;
+    public Text frameText;
+    public Text timeText;
+
+    public int frameRate;
 
     public Frame currentFrameObj;
+    public FrameList frameInfo;
+
+    private int numFrames;
+    private int frame;
+    private float timer;
+    private int currentFrame;
 
     // Start is called before the first frame update
     void Start()
     {
+      if (frameRate==0)
+      {
+        frameRate = 30;
+      }
+
       handleData.GetComponent<handleData>().Init();
       moveBones.GetComponent<MoveBones>().Init();
 
       numFrames = handleData.GetComponent<handleData>().myFrameList.frames.Length;
       frameInfo = handleData.GetComponent<handleData>().myFrameList;
       frame = 0;
+      timer = 0;
+      currentFrame = 0;
 
       frameSlider.minValue = 0;
       frameSlider.maxValue = numFrames - 1;
       frameSlider.wholeNumbers = true;
-
       moveBones.GetComponent<MoveBones>().generateCircles();
     }
 
     // Update is called once per frame
     void Update()
     {
+      timer += Time.deltaTime;
+      timeText.text = "Time: " + timer.ToString();
+
       moveBones.GetComponent<MoveBones>().moveThumb();
 
-      frame = (int) frameSlider.value ;
-      frameText.text = "Frame " + frame.ToString();
+      if (currentFrame < numFrames - 1)
+      {
+          frame = (int) (timer * frameRate); //frameSlider.value;
+      }
 
-      moveBones.GetComponent<MoveBones>().frameToCircle(frameInfo.frames[frame]);
-      currentFrameObj = frameInfo.frames[frame];
+      if (currentFrame != frame){
+        frameSlider.value = frame;
+        frameText.text = "Frame " + frame.ToString();
+        moveBones.GetComponent<MoveBones>().frameToCircle(frameInfo.frames[frame]);
+        currentFrameObj = frameInfo.frames[frame];
+
+        currentFrame = frame;
+      }
+
+
       //moveBones.GetComponent<MoveBones>().generateCircle();
       //JointObj[] hand1 = handleData.GetComponent<handleData>().myFrameList.frames[frame].hand1;
-
-
-      /*
-      if (hand1 != null)
-      {
-        moveBones.GetComponent<MoveBones>().moveHand(hand1);
-      }
-      Debug.Log(moveBones.GetComponent<MoveBones>().thumb.position);
-      */
-
     }
 }
